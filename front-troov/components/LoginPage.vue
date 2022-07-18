588d8f9b1f9ac8d9ffd4c2c9dc4921e2eeb3761f
loubard <yannick.cardini@gmx.com>
2022-07-18 21:53:58 +0700

    first commit :tada:

---
+++ front-troov/components/LoginPage.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="form-login">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Adresse email:"
+        label-for="input-1"
+        description="Nous ne communiquerons jamais votre adresse email à qui que ce soit.."
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          placeholder="Entrez votre email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-2"
+        label="Votre mot de passe:"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          placeholder="Entrez votre mot de passe"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Se connecter</b-button>
+      <!-- <b-button type="reset" variant="danger">Reset</b-button> -->
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: "LoginPage",
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        food: null,
+        checked: [],
+      },
+      foods: [
+        { text: "Select One", value: null },
+        "Carrots",
+        "Beans",
+        "Tomatoes",
+        "Corn",
+      ],
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      // Reset our form values
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.form-login {
+  padding: 5%;
+}
+</style>
