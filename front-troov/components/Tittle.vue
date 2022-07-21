e29624f43ed1000621cae325aca9524d34af5541
loubard <yannick.cardini@gmx.com>
2022-07-21 12:21:42 +0700

    Refonte component tittle :sparkles:

--- front-troov/components/Tittle.vue
+++ front-troov/components/Tittle.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="main-tittle">
-    <h1>
-      Test technique
-      <router-link to="/"><img src="~/assets/logo.png" /></router-link>
-    </h1>
+    <b-row align-v="center">
+      <b-col md="3" lg="2">
+        <router-link to="/"><img src="~/assets/logo.png" /></router-link>
+      </b-col>
+      <b-col md="6" lg="8">
+        <h1>Test technique</h1>
+      </b-col>
+      <b-col md="3" lg="2">
+        <b-link :to="buttonData.route">{{buttonData.text}}</b-link>
+      </b-col>
+    </b-row>
   </div>
 </template>
+
+<script>
+export default {
+  computed:{
+    buttonData() {
+      let currentRoute = this.$route.name;
+      if(currentRoute == 'index')
+        return {text:"Se connecter",route:"/connexion"}
+      else if(currentRoute == 'connexion')
+        return {text:"S'inscrire",route:"/"}
+      else
+        return {text:"Se déconnecter",route:"/connexion"}
+    },
+  }
+};
+</script>
 
 <style>
 .main-tittle {
